1381c459a89eed6a24e2763d7b88fcc39757d490
Feelsrat <juriraak98@gmail.com>
2026-04-29 11:25:47 +0100

    remove temp_extract folder, was just a test thing

--- temp_extract/defaults.py
+++
@@ -1,8 +0,0 @@
-"""
-Decky Task Manager - Default Settings
-
-This file can be used to define default settings for the plugin.
-Currently, the plugin doesn't require any default settings.
-"""
-
-# Default settings (empty for now)
